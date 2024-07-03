97fad91470f03d6a71f54961901e0c9fe82f58f3
dasdevangshu <dasdevangshu104@gmail.com>
2024-07-04 02:55:01 +0530

    changed css in Chat Request, hover colour change issues

--- app/components/ChatRequest.tsx
+++ app/components/ChatRequest.tsx
@@ -2,11 +2,11 @@
 
 import { useRef, useState } from "react"
 
-const acceptIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 hover:text-green-600 text-slate-500 dark:text-teal-50 hover:scale-110 transition duration-300 ease-in-out">
+const acceptIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 hover:text-green-600 dark:hover:text-green-600 text-slate-500 dark:text-teal-50 hover:scale-110 transition duration-300 ease-in-out">
     <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
 
-const rejectIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 hover:text-red-600 text-slate-500 dark:text-teal-50 hover:scale-110 transition duration-300 ease-in-out">
+const rejectIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 hover:text-red-600 dark:hover:text-red-600 text-slate-500 dark:text-teal-50 hover:scale-110 transition duration-300 ease-in-out">
     <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
 
